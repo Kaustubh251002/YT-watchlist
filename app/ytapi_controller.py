bb83c7631eefa6c824881da87c03a556b7282750
Kaustubh251002 <kmishra2510@gmail.com>
2024-01-30 00:37:16 +0530

    Remove exposed key

--- app/ytapi_controller.py
+++ app/ytapi_controller.py
@@ -1,10 +1,32 @@
 from googleapiclient.discovery import build
 import os
 
-yt_key = os.environ.get('YOUTUBE_API_KEY')
+yt_key = os.environ.get('YOUTUBE_API_KEY').split(',')
+
 
 def get_latest_videos(query):
     
+    youtube = build('youtube', 'v3', developerKey=yt_key)
+
+    # Make a search request to get the latest videos
+    request = youtube.search().list(
+        q=query,
+        part='id,snippet',
+        order='date',
+        type='video',
+        maxResults=10  # Adjust the number of results as needed
+    )
+
+    response = request.execute()
+
+    # Extract relevant information from the API response
     videos = []
-    
-    return videos
+    for item in response.get('items', []):
+        video_info = {
+            'video_id': item['id']['videoId'],
+            'title': item['snippet']['title'],
+            'description': item['snippet']['description'],
+            'publish_datetime': item['snippet']['publishedAt'],
+            'thumbnails': item['snippet']['thumbnails']
+        }
+        videos.append(video_info)
